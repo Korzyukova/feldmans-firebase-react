c9bd96f7b98493e6ea18050156735a06195d4bb7
Tatiana Korzyukova <korzioukova@gmail.com>
2023-06-21 19:08:33 -0400

    footer start

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import Header from "../../components/Header/Header";
 import WhatWeDo from "../../components/WhatWeDo/WhatWeDo";
 import Footer from "../../components/Footer/Footer";
 
-import "../Home/Home.css";
+import "./Home.css";
 export default function Home() {
     return (
       <>
