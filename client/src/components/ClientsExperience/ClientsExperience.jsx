37e52a615593eeccebcfbd7f60c691bc1275884a
Tatiana Korzyukova <korzioukova@gmail.com>
2023-07-07 11:30:53 -0400

    clients and experience

--- client/src/components/ClientsExperience/ClientsExperience.jsx
+++ client/src/components/ClientsExperience/ClientsExperience.jsx
@@ -4,18 +4,25 @@ export default function ClientsExperience() {
     return (
       <>
         <div className="clientsexperience">
-            <h1 className="first">OUR STORY</h1>
-            <h2 className="second">Feldmans Advocacy Group was founded in 2017. It is named after Grigoriy Feldman, a Soviet mathematician and economist, persecuted by the Soviet government led by Joseph Stalin. It is because of this foundation that we are committed to causes of democracy, human and civil rights, and justice.</h2>
+            <h2 className="second">Feldmans Advocay Group was founded in 2017 to help candidates we believe in be successful in reaching out to potential voters, developing and spreading a concise campaign message and strong platform, and presenting themselves and their campaigns as viable and competitive.
+
+In addition to political campaigns, we work with non-profit, social advocacy, and political organizations whose missions we believe in to help them reach their highest potential through well-developed and effective web and social media outreach strategies.</h2>
         
-            <h1 className="first">WHO WE ARE</h1>
+            <h1 className="first">POLITICAL CONSULTING</h1>
         
 <ul>
-<li>Pavel Ivlev, President & Owner</li>
-<li>Andrey Romanov, Senior Associate & Co-Owner</li>
-<li>Jack Legore, Senior Associate & Co-Owner</li>
-<li><li>Jack Legore, Senior Associate & Co-Owner</li></li>
+<li>Michael Helfrich for York City Mayor (City of York, PA)</li>
+<li>Steve Snell for PA House of Representatives (PA-LD 94)</li>
+<li>Mayor Shawn Mauck for State Senate (PA-SD 28)</li>
+<li>Shavonnia Corbin-Johnson for Congress (PA-CD 10)</li>
 </ul>
 
+<h1 className="first">NON-PROFIT & SOCIAL ADVOCACY CONSULTING</h1>
+        
+<ul>
+<li>Bro2Go Ex-Offender Program and Brotherhood (York, PA)</li>
+
+</ul>
         </div>
       </>
     );
