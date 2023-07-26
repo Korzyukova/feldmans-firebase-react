ed98f5894130453adc5aea3bd26f7cafdfa5737f
Tatiana Korzyukova <korzioukova@gmail.com>
2023-07-25 21:06:44 -0400

    burger probably not correct

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
 import './Header.css'
 import logo from '../../images/logo.png'
 import city from '../../images/city.png'
+import { useState, useRef } from 'react'
 export default function Header() {
   const links = {
     link1: '/',
     link2: '/aboutus',
     link3: '/clients',
     link4: '/contact-us',
+  }
+
+  const menuRef = useRef(null)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
   }
   return (
     <>
@@ -26,7 +34,29 @@ export default function Header() {
             Contact Us
           </a>
         </div>
+        <div className="burger-menu" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
       </header>
+      <div className={`mobile-menu ${isMenuOpen ? 'open' : ''}`} ref={menuRef}>
+        <a className="link" href={links.link1}>
+          Home
+        </a>
+        <a className="link" href={links.link2}>
+          About Us
+        </a>
+        <a className="link" href={links.link3}>
+          Clients & Experience
+        </a>
+        <a className="link" href={links.link4}>
+          Contact Us
+        </a>
+        <div className="close-menu" onClick={toggleMenu}>
+          &#10005;
+        </div>
+      </div>
       <img className="header-picture" alt="city" src={city} />
     </>
   )
